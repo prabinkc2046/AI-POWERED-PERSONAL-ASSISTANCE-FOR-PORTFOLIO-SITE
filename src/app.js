39441b71955d01a483ea3c5a17bf7c84a334c1ac
Prabin K C <pkmiracle36@gmail.com>
2024-12-04 15:17:09 +1100

    testing with dev mode

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import './config/env.js';
 import corsMiddleware from './middlewares/cors.middleware.js';
 import express from 'express';
+import cors from 'cors';
 
 import ChatRoutes from './routes/chatRoute.js';
 
 const app = express();
 
 // apply cors policy middleware
-app.use(corsMiddleware);
+// app.use(corsMiddleware);
+app.use(cors());
 
 // parse json body
 app.use(express.json());
@@ -16,9 +18,9 @@ app.use(express.json());
 app.use('/', ChatRoutes);
 
 const PORT = process.env.PORT || 3000;
-const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+const HOST = process.env.NODE_ENV === 'development' ? '0.0.0.0' : 'localhost';
 app.listen(PORT, HOST, () => {
-  if (process.env.NODE_ENV === 'production') {
+  if (process.env.NODE_ENV === 'development') {
     console.log(`Server running on port: ${PORT}`);
   } else {
     console.log(`Server running on http://localhost:${PORT}`);
